e15ee2ba5b8b73f3fb92b955edb001ee8db35b8f
Joseph Kempster <jkempster34@gmail.com>
2020-08-12 21:47:18 +0100

    Get user latitude and longitude

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import MapView from "react-native-maps";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet, View, Dimensions, Text } from "react-native";
 
-const Map = () => {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.mapStyle} />
-    </View>
-  );
-};
+export default class Map extends Component {
+  state = {
+    location: {
+      latitude: 0,
+      longitude: 0,
+    },
+  };
+
+  getCurrentPosition = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { coords } = position;
+      const { latitude, longitude } = coords;
+      const location = {
+        latitude: latitude,
+        longitude: longitude,
+      };
+      if (location !== null) this.setState({ location });
+    });
+  };
+
+  render() {
+    const { location } = this.state;
+    const { latitude, longitude } = location;
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.mapStyle}
+          onUserLocationChange={this.getCurrentPosition}
+          showsUserLocation={true}
+          followsUserLocation={true}
+        />
+        <View style={styles.textStyle}>
+          <Text>{`lat: ${latitude}`}</Text>
+          <Text>{`long: ${longitude}`}</Text>
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -21,6 +53,10 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
   },
+  textStyle: {
+    position: "absolute",
+    backgroundColor: "red",
+    top: 50,
+    width: 200,
+  },
 });
-
-export default Map;
