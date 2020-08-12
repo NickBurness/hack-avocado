c947e171b694dbd2c20bfb84dbecf2b0863aa790
Joseph Kempster <jkempster34@gmail.com>
2020-08-12 17:55:45 +0100

    Add MapView following https://docs.expo.io/versions/latest/sdk/map-view/

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,8 +1,26 @@
 import React from "react";
-import { Text } from "react-native";
+import MapView from "react-native-maps";
+import { StyleSheet, View, Dimensions } from "react-native";
 
 const Map = () => {
-  return <Text>At the map</Text>;
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.mapStyle} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
 
 export default Map;
